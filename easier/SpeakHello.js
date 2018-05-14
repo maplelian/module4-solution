ac1282a23b192a4beea139f54c7adf25793d5f8b
maplelian <39064446+maplelian@users.noreply.github.com>
2018-05-14 14:39:18 -0700

    Delete SpeakHello.js

--- easier/SpeakHello.js
+++
@@ -1,9 +0,0 @@
-(function(window){
-  var helloSpeakWord = "Hello";
-  var helloSpeaker={};
-  helloSpeaker.helloSpeak=function(names) {
-    console.log(helloSpeakWord+ " " + names[i]);
-  }
-window.helloSpeaker = helloSpeaker
-
-})(window);
