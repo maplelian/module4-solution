b658df7bdf827dbc2dea05158e9d53e19da85974
maplelian <kate.lian@outlook.com>
2018-05-14 14:37:34 -0700

    Coursera

---
+++ SpeakGoodBye.js
@@ -0,0 +1,8 @@
+(function(window){
+  var byeSpeakWord = "Good Bye";
+  var byeSpeaker={};
+    byeSpeaker.byeSpeak=function(names) {
+    console.log(byeSpeakWord+ " "+names[i]);
+    }
+window.byeSpeaker = byeSpeaker;
+})(window);
---
+++ SpeakHello.js
@@ -0,0 +1,9 @@
+(function(window){
+  var helloSpeakWord = "Hello";
+  var helloSpeaker={};
+  helloSpeaker.helloSpeak=function(names) {
+    console.log(helloSpeakWord+ " " + names[i]);
+  }
+window.helloSpeaker = helloSpeaker
+
+})(window);
