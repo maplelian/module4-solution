92afbfe79a1c05eb005afaadeb3d35b1a87f806a
maplelian <39064446+maplelian@users.noreply.github.com>
2018-05-14 14:39:32 -0700

    Delete script.js

--- easier/script.js
+++
@@ -1,9 +0,0 @@
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-for (i = 0; i<names.length; i++){
-  var firstLetter = names[i].charAt(0).toLowerCase();
-  if (firstLetter === 'j') {
-    byeSpeaker.byeSpeak(names);
-  } else {
-    helloSpeaker.helloSpeak(names);
-  }
-}
