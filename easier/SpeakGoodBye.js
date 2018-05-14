10c17b2160cbbdfd0183066a4b68518fd900f96c
maplelian <39064446+maplelian@users.noreply.github.com>
2018-05-14 14:39:09 -0700

    Delete SpeakGoodBye.js

--- easier/SpeakGoodBye.js
+++
@@ -1,8 +0,0 @@
-(function(window){
-  var byeSpeakWord = "Good Bye";
-  var byeSpeaker={};
-    byeSpeaker.byeSpeak=function(names) {
-    console.log(byeSpeakWord+ " "+names[i]);
-    }
-window.byeSpeaker = byeSpeaker;
-})(window);
